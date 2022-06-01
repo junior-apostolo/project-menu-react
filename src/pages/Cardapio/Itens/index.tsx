56fe77fc9f2f8dd2f046bbdf192bca4e02f031df
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-06-01 15:38:35 -0300

    criando componentes Item e Itens para o cardapio

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,13 @@
+import itens from './itens.json'
+import Item from './Item'
+import styles from './Itens.module.scss'
+
+export default function Itens() {
+  return (
+    <div className={styles.itens}>
+      {itens.map((item) => (
+        <Item key={item.id}/>
+      ))}
+    </div>
+  )
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import Search from './Buscador'
 import { useState } from 'react'
 import Filters from './Filtros'
 import Order from './Ordenador'
+import Itens from './Itens'
 
 export default function Main() {
   const [busca, setBusca] = useState("")
@@ -25,8 +26,9 @@ export default function Main() {
         <Search busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filters filtro={filtro} setFiltro={setFiltro} />
-          <Order order={order} setOrder={setOrder}/>
+          <Order order={order} setOrder={setOrder} />
         </div>
+        <Itens />
       </section>
     </main>
   )

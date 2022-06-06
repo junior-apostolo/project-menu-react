762309da473c3170c44fbfdb8e470a68d70971ec
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-06-06 12:31:50 -0300

    criando ordenador

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export default function Itens(props: Props) {
   const [lista, setLista] = useState(itens)
-  const { busca, filtro } = props
+  const { busca, filtro, order } = props
 
   function testaBusca(title: string) {
     const regex = new RegExp(busca, 'i')
@@ -23,10 +23,22 @@ export default function Itens(props: Props) {
     if(filtro !== null) return filtro === id
   }
 
+  function ordenar(newList: typeof itens){
+    switch(order){
+      case 'porcao':
+        return newList.sort((a, b) => a.size > b.size ? 1 : -1)
+      case 'qtd_pessoas':
+        return newList.sort((a, b) => a.serving > b.serving ? 1 : -1)
+      case 'preco':
+        return newList.sort((a, b) => a.price > b.price ? 1 : -1)
+    }
+    return newList
+  }
+
   useEffect(() => {
     const newList = itens.filter(item => testaBusca(item.title) && testaFiltro(item.category.id))
-    setLista(newList)
-  }, [busca, filtro])
+    setLista(ordenar(newList))
+  }, [busca, filtro, order])
 
   return (
     <div className={styles.itens}>

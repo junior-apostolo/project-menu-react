2eacf9c72192ec46ccf3786b6d3669fda3ae6c89
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-06-06 13:07:05 -0300

    refatorando ordenação

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -15,28 +15,33 @@ export default function Itens(props: Props) {
 
   function testaBusca(title: string) {
     const regex = new RegExp(busca, 'i')
-
     return regex.test(title)
   }
 
-  function testaFiltro(id: number){
-    if(filtro !== null) return filtro === id
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id
+    return true
   }
 
-  function ordenar(newList: typeof itens){
-    switch(order){
+  function ordencaoCrescente(array: typeof itens, prop: 'size' | 'serving' | 'price'){
+    return array.sort((a, b) => (a[prop] > b[prop] ? 1 : -1))
+  }
+
+  function ordenar(newList: typeof itens) {
+    switch (order) {
       case 'porcao':
-        return newList.sort((a, b) => a.size > b.size ? 1 : -1)
+        return ordencaoCrescente(newList, 'size')
       case 'qtd_pessoas':
-        return newList.sort((a, b) => a.serving > b.serving ? 1 : -1)
+        return ordencaoCrescente(newList, 'serving')
       case 'preco':
-        return newList.sort((a, b) => a.price > b.price ? 1 : -1)
+        return ordencaoCrescente(newList, 'price')
+      default:
+        return newList;
     }
-    return newList
   }
 
   useEffect(() => {
-    const newList = itens.filter(item => testaBusca(item.title) && testaFiltro(item.category.id))
+    const newList = itens.filter((item) => testaBusca(item.title) && testaFiltro(item.category.id))
     setLista(ordenar(newList))
   }, [busca, filtro, order])
 
